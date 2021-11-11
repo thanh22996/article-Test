7b298926002007d0aacb7ff07644d2e75c5e17c5
thanhns <thanhns@gsoft.com.vn>
2021-11-11 22:02:28 +0700

    fix layout recruitment

--- src/components/Recruitment.js
+++ src/components/Recruitment.js
@@ -4,7 +4,11 @@ function Recruitment(props) {
   return (
     <div className="page-recruitment">
       <div className="top-header">
-        <img src="/assets/images/recruitment/top-header.png" alt="picHeader" />
+        <img
+          width="100%"
+          src="/assets/images/recruitment/top-header.png"
+          alt="picHeader"
+        />
       </div>
       <div className="bottom-header">
         <div className="container">
@@ -16,19 +20,57 @@ function Recruitment(props) {
             <span className="bl-sub-br">Tuyển dụng</span>
           </div>
           <p className="bl-title">Tuyển dụng</p>
-          <p>Các vị trí đang tuyển dụng</p>
+          <p className="top-title-name">Các vị trí đang tuyển dụng</p>
           <hr />
         </div>
       </div>
       <div className="block-content">
         <div className="container">
-          <div className="row">
+          <div className="row wrapper-recruitment">
             <div className="col-md-4">
-              <div className="img-recruitment">
-                <img src="" alt="" />
+              <div className="block-recruitment">
+                <div className="img-recruitment">
+                  <img
+                    width="100%"
+                    src="/assets/images/recruitment/img_top.png"
+                    alt="picHead"
+                  />
+                </div>
+                <div className="content-recruitment">
+                  <p className="bl-title-recruitment">Tuyển dụng</p>
+                  <p className="bl-name">Nhân viên kinh doanh</p>
+                  <p className="bl-description">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Quisque molestie lorem euconsequat gravida. Nunc mattis et
+                    erat eu sagittis. Fusce in arcu ullamcorper, fermentum nulla
+                    et, pretium ipsum.
+                  </p>
+                  <p className="bl-read-more">Tìm hiểu công việc</p>
+                </div>
               </div>
             </div>
-            <div className="col-md-4"></div>
+            <div className="col-md-4">
+              <div className="block-recruitment">
+                <div className="img-recruitment">
+                  <img
+                    width="100%"
+                    src="/assets/images/recruitment/img_top.png"
+                    alt="picHead"
+                  />
+                </div>
+                <div className="content-recruitment">
+                  <p className="bl-title-recruitment">Tuyển dụng</p>
+                  <p className="bl-name">Nhân viên kinh doanh</p>
+                  <p className="bl-description">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Quisque molestie lorem euconsequat gravida. Nunc mattis et
+                    erat eu sagittis. Fusce in arcu ullamcorper, fermentum nulla
+                    et, pretium ipsum.
+                  </p>
+                  <p className="bl-read-more">Tìm hiểu công việc</p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>

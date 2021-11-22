463dd568034bcab23f930ee5b83480a58abd3157
Thành Nguyễn Sinh <thanhns@Ggroup.vn>
2021-11-22 17:31:37 +0700

    fix intro page

--- src/components/IntroducePage.js
+++ src/components/IntroducePage.js
@@ -30,6 +30,95 @@ function IntroducePage(props) {
             ích cho khách hàng.
           </p>
           <p className="bl-title">Quá trình thành lập & phát triễn</p>
+          <div className="bl-content-intro">
+            <div className="bl-item-intro">
+              <div className="row">
+                <div className="col-md-3">
+                  <p className="bl-year">08.12.2020</p>
+                </div>
+                <div className="col-md-9 bl-content">
+                  <p>Thành lập công ty TNHH BANKUS</p>
+                  <p>
+                    Với độ ngũ nhân viên vỏn vẹn 15 TSA và 2 quản lý cùng triển
+                    khai dự án EasyCredit và Mirae Asset theo mô hình 3P- DSA
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="bl-item-intro">
+              <div className="row">
+                <div className="col-md-3">
+                  <p className="bl-year">31.08.2020</p>
+                </div>
+                <div className="col-md-9 bl-content">
+                  <p>Nguồn nhân lực đạt mức 50 sale – 4 quản lý</p>
+                  <p>
+                    Sau 2 tháng hoạt động đã thúc đẩy ổn định nguồn nhân sự và
+                    sẵn sàng mở rộng dự án triển khai.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="bl-item-intro">
+              <div className="row">
+                <div className="col-md-3">
+                  <p className="bl-year">01.12.2020</p>
+                </div>
+                <div className="col-md-9 bl-content">
+                  <p>Kí kết dự án SHBFinance và PTF</p>
+                  <p>
+                    Đưa tổng dự án triển khai hiện tại lên 3 dự án, cùng với đó
+                    là sự chuẩn bị nhân lực cho năm 2021 đầy kỳ vọng.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="bl-item-intro">
+              <div className="row">
+                <div className="col-md-3">
+                  <p className="bl-year">01.04.2021</p>
+                </div>
+                <div className="col-md-9 bl-content">
+                  <p>Mở rộng sàn Telesale</p>
+                  <p>
+                    Sau khi hoạt động sàn 1, đội Telesale ngày càng phát triển
+                    về lượng nhân sự. Công ty mở rộng thêm sàn 2 tại cùng địa
+                    chỉ để có môi trường hoạt động tốt nhất cho Telesale
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="bl-item-intro">
+              <div className="row">
+                <div className="col-md-3">
+                  <p className="bl-year">26.10.2021</p>
+                </div>
+                <div className="col-md-9 bl-content">
+                  <p>Thành lập chi nhánh tại An Giang</p>
+                  <p>
+                    Việc mở rộng chi nhánh đánh dấu mốc quan trọng trong quá
+                    trình phát triển của BANKUS. Mở rộng khu vực triển khai ra
+                    toàn Miền Nam và MêKong.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="bl-item-intro">
+              <div className="row">
+                <div className="col-md-3">
+                  <p className="bl-year">01/11/2021</p>
+                </div>
+                <div className="col-md-9 bl-content">
+                  <p>Kí kết triển khai trực tiếp dự án Mirae Asset-TSA</p>
+                  <p>
+                    Sau một thời gian liên kết công ty về dự án MAFC, Bankus đã
+                    triển khai hoạt động trực tiếp với Mirae Asset trong dự án
+                    MAFC-TSA.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import "./style/index.css";
+import "./style/intro.css";
 import App from "./App";
 
 import { Provider } from "react-redux";
